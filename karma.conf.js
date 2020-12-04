9cbeff2b0dfc95076ac8ae39c9d456dc5fe19b27
Andrew L Jones <ajones@digitalbazaar.com>
2020-12-04 16:13:43 -0500

    Add compatibility spec.

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     ],
 
     // list of files to exclude
-    exclude: [],
+    exclude: ['test/compatibility.spec.js'],
 
     // preprocess matching files before serving them to the browser
     // preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
