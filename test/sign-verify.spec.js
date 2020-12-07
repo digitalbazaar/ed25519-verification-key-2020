4fd575c75bbeafe1e2c9523e7bf01c7891e38f09
Andrew L Jones <ajones@digitalbazaar.com>
2020-12-07 15:56:07 -0500

    Add tests that run in browser and node using base58 encoded signatures.

--- test/sign-verify.spec.js
+++ test/sign-verify.spec.js
@@ -5,7 +5,7 @@ import chai from 'chai';
 chai.should();
 
 import {Ed25519VerificationKey2020} from '../';
-import {mockKey, mockNodeKey} from './mock-data.js';
+import {mockKey, suites} from './mock-data.js';
 import {stringToUint8Array} from './text-encoder.js';
 import * as base58btc from 'base58-universal';
 
@@ -38,19 +38,19 @@ describe('sign and verify', () => {
     const result = await verifier.verify({data: changedData, signature});
     result.should.be.false;
   });
-
-  // this simulates what happens when a backend key creates a signature
-  // and it is verified on both the backend and the front end
-  it('verifies with key & signature produced using node crypto', async () => {
-    const _keyPair = new Ed25519VerificationKey2020({
-      controller: 'did:example:1234',
-      ...mockNodeKey
+  // these tests simulate what happens when a key & signature
+  // created in either the browser or the node is verified
+  // in a different enviroment
+  for(const suite of suites) {
+    it(suite.title, async () => {
+      const _keyPair = new Ed25519VerificationKey2020({
+        controller: 'did:example:1234',
+        ...suite.key
+      });
+      const data = stringToUint8Array(suite.data);
+      const signature = base58btc.decode(suite.signature);
+      const result = await _keyPair.verifier().verify({data, signature});
+      result.should.be.true;
     });
-    const data = stringToUint8Array(mockNodeKey.data);
-    // this signature was produced by node's crypto.sign function
-    // and is base58 encoded
-    const signature = base58btc.decode(mockNodeKey.signature);
-    const result = await _keyPair.verifier().verify({data, signature});
-    result.should.be.true;
-  });
+  }
 });
