50c9ceb1c5f668a7c400ef05974b597044926841
Andrew L Jones <ajones@digitalbazaar.com>
2020-12-07 15:32:20 -0500

    Add test for node key verification on both back and front end.

--- test/sign-verify.spec.js
+++ test/sign-verify.spec.js
@@ -5,7 +5,7 @@ import chai from 'chai';
 chai.should();
 
 import {Ed25519VerificationKey2020} from '../';
-import {mockKey} from './mock-data.js';
+import {mockKey, mockNodeKey} from './mock-data.js';
 import {stringToUint8Array} from './text-encoder.js';
 import * as base58btc from 'base58-universal';
 
@@ -37,5 +37,20 @@ describe('sign and verify', () => {
     const changedData = stringToUint8Array('test 4321');
     const result = await verifier.verify({data: changedData, signature});
     result.should.be.false;
+  });
+
+  // this simulates what happens when a backend key creates a signature
+  // and it is verified on both the backend and the front end
+  it('verifies with key & signature produced using node crypto', async () => {
+    const _keyPair = new Ed25519VerificationKey2020({
+      controller: 'did:example:1234',
+      ...mockNodeKey
+    });
+    const data = stringToUint8Array(mockNodeKey.data);
+    // this signature was produced by node's crypto.sign function
+    // and is base58 encoded
+    const signature = base58btc.decode(mockNodeKey.signature);
+    const result = await _keyPair.verifier().verify({data, signature});
+    result.should.be.true;
   });
 });
