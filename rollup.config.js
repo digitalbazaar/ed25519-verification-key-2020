306dafdb7c6588a4b18b99a5442154dd6ad74ddc
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-05-05 22:24:34 -0400

    Include the -browser file in dist/.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import pkg from './package.json';
 
-export default {
-  input: './lib/index.js',
-  output: [
-    {
-      dir: 'dist',
-      format: 'cjs',
-      preserveModules: true
-    }
-  ],
-  plugins: [
-  ],
-  external: Object.keys(pkg.dependencies).concat(['crypto', 'util']),
-};
+export default [
+  {
+    input: './lib/index.js',
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+        preserveModules: true
+      }
+    ],
+    external: Object.keys(pkg.dependencies).concat(['crypto', 'util'])
+  },
+  {
+    input: './lib/ed25519-browser.js',
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+        preserveModules: true
+      }
+    ],
+    external: Object.keys(pkg.dependencies).concat(['crypto', 'util'])
+  }
+];
