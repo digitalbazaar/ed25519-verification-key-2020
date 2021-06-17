d6b59a49e84d2928a83ea29e93cdb15c7c914dc5
Manu Sporny <msporny@digitalbazaar.com>
2021-06-16 20:53:19 -0400

    Fix sign and verify tests.

--- test/sign-verify.spec.js
+++ test/sign-verify.spec.js
@@ -19,15 +19,15 @@ const verifier = keyPair.verifier();
 
 // the same signature should be generated on every test platform
 // (eg. browser, node14)
-const targetSignatureBase58 = '57PG4Ahy97k8iwmRVf8bEK9ZXuy8Q7wz3Mx' +
-  'BQkwrNE5jsGaiWdzYnEK1SiP8yZ4VfEujd4FCkfxzUaBQQEZzL6PK';
+const targetSignatureBase58 = '25fgrioJMRbKuq4sGz2Ngh6K7GuonRTUAzRk7asgvnVA2W' +
+  'YSHtLBPX1BXiTtMqTwen7MKgQfbMpm6N6vgDc7VDF9';
 
 describe('sign and verify', () => {
   it('works properly', async () => {
     signer.should.have.property('id',
-      'did:example:1234#z6MkszZtxCmA2Ce4vUV132PCuLQmwnaDD5mw2L23fGNnsiX3');
+      'did:example:1234#z6MknCCLeeHBUaHu4aHSVLDCYQW9gjVJ7a63FpMvtuVMy53T');
     verifier.should.have.property('id',
-      'did:example:1234#z6MkszZtxCmA2Ce4vUV132PCuLQmwnaDD5mw2L23fGNnsiX3');
+      'did:example:1234#z6MknCCLeeHBUaHu4aHSVLDCYQW9gjVJ7a63FpMvtuVMy53T');
     const data = stringToUint8Array('test 1234');
     const signature = await signer.sign({data});
     base58btc.encode(signature).should.equal(targetSignatureBase58);
@@ -51,6 +51,7 @@ describe('sign and verify', () => {
         controller: 'did:example:1234',
         ...suite.key
       });
+
       const data = stringToUint8Array(suite.data);
       const signature = base58btc.decode(suite.signature);
       const result = await _keyPair.verifier().verify({data, signature});
